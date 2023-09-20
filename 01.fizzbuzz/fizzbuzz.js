2741866e4ddb373d4d9b45ec5c2ac94b01833293
GithubMuftah <muftah@i3-systems.com>
2023-09-20 18:06:03 +0900

    initial push

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let fizzbuzz_number = 1; fizzbuzz_number <= 20; fizzbuzz_number++) {
+  if (fizzbuzz_number % 3 == 0 && fizzbuzz_number % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (fizzbuzz_number % 3 == 0) {
+    console.log("Fizz");
+  } else if (fizzbuzz_number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(fizzbuzz_number);
+  }
+}
