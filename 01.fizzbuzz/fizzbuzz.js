7a3d61cde63a87d0e06de2d082fc91df7ec5138f
GithubMuftah <muftah@i3-systems.com>
2023-09-22 16:59:45 +0900

    minor fixes

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,9 +1,9 @@
 for (let fizzbuzzNumber = 1; fizzbuzzNumber <= 20; fizzbuzz_number++) {
-  if (fizzbuzz_number % 3 ==== 0 && fizzbuzz_number % 5 ==== 0) {
+  if (fizzbuzz_number % 3 === 0 && fizzbuzz_number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (fizzbuzz_number % 3 ==== 0) {
+  } else if (fizzbuzz_number % 3 === 0) {
     console.log("Fizz");
-  } else if (fizzbuzz_number % 5 ==== 0) {
+  } else if (fizzbuzz_number % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(fizzbuzz_number);
