0ebb2147deafd5acfcc3603d1ae540621d1531be
GithubMuftah <muftah@i3-systems.com>
2023-09-22 10:05:36 +0900

    fix equal operands and variable naming

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
-for (let fizzbuzz_number = 1; fizzbuzz_number <= 20; fizzbuzz_number++) {
-  if (fizzbuzz_number % 3 == 0 && fizzbuzz_number % 5 == 0) {
+for (let fizzbuzzNumber = 1; fizzbuzzNumber <= 20; fizzbuzzNumber++) {
+  if (fizzbuzzNumber % 3 === 0 && fizzbuzzNumber % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (fizzbuzz_number % 3 == 0) {
+  } else if (fizzbuzzNumber % 3 === 0) {
     console.log("Fizz");
-  } else if (fizzbuzz_number % 5 == 0) {
+  } else if (fizzbuzzNumber % 5 === 0) {
     console.log("Buzz");
   } else {
-    console.log(fizzbuzz_number);
+    console.log(fizzbuzzNumber);
   }
 }
